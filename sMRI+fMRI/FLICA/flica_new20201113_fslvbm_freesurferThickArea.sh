9e15963d83b3ef77770d96f3e614c5564be23313
Jiyang Jiang <54608573+JiyangJiang@users.noreply.github.com>
2020-11-17 15:04:12 +1100

    Update flica_new20201113_fslvbm_freesurferThickArea.sh

--- sMRI+fMRI/FLICA/flica_new20201113_fslvbm_freesurferThickArea.sh
+++ sMRI+fMRI/FLICA/flica_new20201113_fslvbm_freesurferThickArea.sh
@@ -172,3 +172,56 @@ opts.calcFits = 'all'; % Be more careful, check F increases every iteration.
 Morig = flica(Y, opts);
 [M,weights] = flica_reorder(Morig); % Sort components sensibly
 flica_save_everything(outdir, M, fileinfo);
+
+% post hoc correlation
+% Jmod - this part is different from Smith 2019 code.
+%        this part is according to online FLICA instruction.
+clear des
+des.Subject_Index = (1:size(Y{1},2))';
+des.Age = load('/data4/jiyang/MW24+SCS_FLICA/flica/covariates/age_N310.txt');
+des.Sex = load('/data4/jiyang/MW24+SCS_FLICA/flica/covariates/sex_N310.txt');
+des.ICV = load('/data4/jiyang/MW24+SCS_FLICA/flica/covariates/eTIVfromFreeSurfer_N310.txt');
+flica_posthoc_correlations(outdir, des)
+
+
+# copy mgh to NiL cluster to generate freesurfer figures
+# ------------------------------------------------------
+outdir=/data4/jiyang/MW24+SCS_FLICA/flica/flica_new20201113_fslvbm_freesurferThickArea
+rsync -avrPS ${outdir}/*.mgh jiyang@129.94.18.57:/home/jiyang/Downloads/.
+ssh -XY jiyang@129.94.18.57
+cd ~/Downloads
+export PATH=/home/jiyang/my_software/flica_Jmod:$PATH
+render_surfaces.sh
+surfaces_to_volumes_all.sh $(pwd)/fsaverage $FSLDIR/data/standard/MNI152_T1_2mm.nii.gz
+
+# copy generated files on NiL cluster back to GRID to generate html reports
+# -------------------------------------------------------------------------
+exit
+cd ${outdir}
+rsync -avrPS jiyang@129.94.18.57:~/Downloads/* .
+export PATH=/home/jiyang/Software/flica_Jmod:$PATH
+render_lightboxes_all.sh
+flica_html_report.sh
+
+# Calculation of the variance explained
+# Jmod : calculation based on low resolution results, as high res is only for display
+# -----------------------------------------------------------------------------------
+cd ${outdir}
+fslmaths niftiOut_mi1.nii.gz -sqr tmp1.nii.gz  
+fslstats -t tmp1.nii.gz -m > energy1.txt
+fslmaths niftiOut_mi2.nii.gz -sqr tmp2.nii.gz  
+fslstats -t tmp2.nii.gz -m > energy2.txt
+fslmaths niftiOut_mi3.nii.gz -sqr tmp3.nii.gz
+fslstats -t tmp3.nii.gz -m > energy3.txt
+paste energy1.txt energy2.txt energy3.txt > energy_tmp.txt
+
+# in MATLAB
+cd /data4/jiyang/MW24+SCS_FLICA/flica/flica_new20201113_fslvbm_freesurferThickArea/
+energy_tmp = load ('energy_tmp.txt');
+energy = energy_tmp * diag(1./sum(energy_tmp)) * 100;
+fid_energy = fopen('energy.txt','w');
+for i = 1:size(energy,1)
+	fprintf(fid_energy,'%.5f\t%.5f\t%.5f\n',energy(i,1),energy(i,2),energy(i,3));
+end
+
+save ('flica_new20201113_fslvbm_freesurferThickArea','-v7.3');
