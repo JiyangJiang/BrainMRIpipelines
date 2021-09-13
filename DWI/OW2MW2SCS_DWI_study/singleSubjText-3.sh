f706c1d1cb1b6b46813abcfe2f8f7e43d815a6fd
Jiyang Jiang <54608573+JiyangJiang@users.noreply.github.com>
2021-09-13 21:01:53 +1000

    updates

--- DWI/OW2MW2SCS_DWI_study/singleSubjText-3.sh
+++ DWI/OW2MW2SCS_DWI_study/singleSubjText-3.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
+# GROUP LEVEL !!
+
+
 # ---=== Finally note that, for quantitative group studies, a single unique set of 3-tissue response functions should be used for all subjects. 
 # ---=== This can for example be achieved by averaging response functions (per tissue type) across all subjects in the study.
 # Ref: https://mrtrix.readthedocs.io/en/3.0_rc2/fixel_based_analysis/ss_fibre_density_cross-section.html
-# average_response */response.txt ../group_average_response.txt
-
-
-# 3-tissue CSD modelling
-ss3t_csd_beta1 -force dwi_denoised_unringed_preproc_unbiased_upsampled.mif response_wm.txt wmfod.mif response_gm.txt gm.mif response_csf.txt csf.mif -mask dwi_mask_upsampled.mif
-
-# robust bias field correction with info from 3 compartments
-mtnormalise -force wmfod.mif wmfod_norm.mif gm.mif gm_norm.mif csf.mif csf_norm.mif -mask dwi_mask_upsampled.mif
-
-
-# OPTIONAL - visualisation
-fod2dec wmfod_norm.mif decfod.mif -mask dwi_mask_upsampled.mif
+# responsemean */response.txt ../group_responsemean.txt
+work_dir=/home/jiyang/Work/dwi_test
+cd ${work_dir}
+responsemean */response_wm.txt grp_avg_response_wm.txt
+responsemean */response_gm.txt grp_avg_response_gm.txt
+responsemean */response_csf.txt grp_avg_response_csf.txt
