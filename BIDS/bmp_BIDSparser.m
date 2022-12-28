0f0587a0067b11ed6781cf4a897cdbd6e6f9cbdd
Jiyang Jiang <54608573+JiyangJiang@users.noreply.github.com>
2022-12-28 18:36:33 +1100

    BIDS patcher - aslcontext.tsv

--- BIDS/bmp_BIDSparser.m
+++ BIDS/bmp_BIDSparser.m
@@ -11,6 +11,7 @@
 % 28 Nov 2022 - Written by Jiyang Jiang
 
 BIDS_directory = '/sandbox/adni_examples/bids';
+BIDS_directory = '/Users/z3402744/Work/ADNI_test/BIDS';
 
 BMP_PATH     = getenv ('BMP_PATH');
 BMP_3RD_PATH = getenv ('BMP_3RD_PATH');
@@ -18,7 +19,7 @@ addpath (fullfile (BMP_3RD_PATH, 'bids-matlab'));
 addpath (fullfile (BMP_PATH, 'BIDS'));
 
 BIDS_directory_layout = bids.layout('root', BIDS_directory, ...
-				                   'use_schema', true, ...
-				                   'index_derivatives', false, ... % 'derivatives' folder not to be parsed.
-				                   'tolerant', true, ...
-				                   'verbose', true);
+				                   	'use_schema', true, ...
+				                   	'index_derivatives', false, ... % 'derivatives' folder not to be parsed.
+				                   	'tolerant', true, ...
+				                   	'verbose', true);
--- BIDS/bmp_BIDSpatcher.m
+++ BIDS/bmp_BIDSpatcher.m
@@ -1,1 +1,63 @@
-function bmp_BIDSpatcher (BIDS_directory)
+function bmp_BIDSpatcher (varargin)
+%
+%
+% varargin{1} = BIDS directory (default = pwd)
+% varargin{2} = is ADNI? (true/false) (default = true)
+%
+%
+
+	if nargin >= 1 && isfolder(varargin{1})
+
+		BIDS_directory = varargin{1};
+
+	else
+
+		BIDS_directory = pwd;
+
+	end
+
+	if nargin == 2 && islogical(varargin{2})
+
+		isADNI = varargin{2};
+
+	else
+
+		isADNI = true;
+
+	end
+
+
+	% asl context tsv
+	asl_context_tsv (BIDS_directory, isADNI);
+
+	% if exist tsv (clinica) compare with real nii
+	% if not create
+	% asl context tsv
+
+end
+
+
+
+function asl_context_tsv (BIDS_directory, isADNI)
+
+	fprintf ('%s : Creating *_aslcontext.tsv files.\n', mfilename);
+
+	asl_data = bids.query(BIDS_directory,'data','suffix','asl');
+
+	fprintf ('%s : %d ASL scans found.\n', mfilename, size(asl_data,1));
+
+	for i = 1 : size(asl_data,1)
+
+		[curr_dir,curr_fnam,curr_ext] = fileparts(asl_data{i,1});
+
+		fprintf ('%s : Creating aslcontext tsv file for %s ... ', mfilename, [curr_fnam curr_ext]);
+
+		if isADNI
+
+		else
+
+		end
+
+	end
+
+end
