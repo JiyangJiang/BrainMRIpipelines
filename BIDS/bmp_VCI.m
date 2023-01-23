4500343a7f19f0cda8c164a4fe683046833b12bb
Jiyang Jiang <54608573+JiyangJiang@users.noreply.github.com>
2023-01-23 00:04:28 -0500

    Update bmp_VCI.m

--- BIDS/bmp_VCI.m
+++ BIDS/bmp_VCI.m
@@ -7,7 +7,7 @@ function varargout = bmp_VCI (operation_mode, DICOM_directory, varargin)
 %   NIFTI files.
 %
 % ==============================================================
-% USAGE
+%                             USAGE
 % ==============================================================
 %
 %   Run dicomCollection only
@@ -18,15 +18,19 @@ function varargout = bmp_VCI (operation_mode, DICOM_directory, varargin)
 %
 %   Organise DICOM folder
 %   ---------------------
-%   varargin{1} = MAT file with dicomCollection output table. 
-%                 MAT file should have a table named 'dcm_coll' 
-%                 which is output from dicomCollection.
+%
+%   operation_mode = 'o' or 'organise_dicom_dir'
+%
+%   varargin{1} = MAT file containing dicomCollection output table. 
+%                 Table should have the name 'dcm_coll'.
 %
 %                 OR
 %
-%                 The table output from dicomCollection.
+%                 The dicomCollection output table.
 %
-% HISTORY
+% ==============================================================
+%                          HISTORY
+% ==============================================================
 %   
 
 	BMP_TMP_PATH = getenv ('BMP_TMP_PATH');
@@ -62,12 +66,6 @@ function varargout = bmp_VCI (operation_mode, DICOM_directory, varargin)
 
 			DICOM_parent_folder = fileparts (DICOM_directory);
 
-			modalities = {
-							'T1'
-							'T2'
-							'FLAIR'
-						};
-
 			if nargin == 2
 				dcm_coll = load (fullfile (BMP_TMP_PATH,'vci_dcm_coll.mat')).dcm_coll;
 			elseif nargin == 3 && ischar(varargin{1}) && isfile (varargin{1})
@@ -76,15 +74,28 @@ function varargout = bmp_VCI (operation_mode, DICOM_directory, varargin)
 				dcm_coll = varargin{1};
 			end
 
-			% for i = 1 : length(modalities)
-			% 	orgniseDICOMdir (dcm_coll, DICOM_parent_folder, modalities{i,1}, str_toBeReplaced, str_replaceTo);
-			% end	
+			modalities = {
+							'T1'
+							'T2'
+							'FLAIR'
+							'SWI'
+							'DTI'
+							'ASL'
+							'CVR_fmap'
+							'CVR'
+							'MP2RAGE'
+							'DCE'
+						};
 
-			orgniseDICOMdir_dwi	(dcm_coll, DICOM_parent_folder, str_toBeReplaced, str_replaceTo);
+			for i = 1 : length(modalities)
+				orgniseDICOMdir (dcm_coll, DICOM_parent_folder, modalities{i,1}, str_toBeReplaced, str_replaceTo);
+			end	
+
 
 	end
 
 end
+
 
 
 function orgniseDICOMdir (dcm_coll, DICOM_parent_folder, modality, str_toBeReplaced, str_replaceTo)
@@ -103,106 +114,91 @@ function orgniseDICOMdir (dcm_coll, DICOM_parent_folder, modality, str_toBeRepla
 
 			series_description_keyword = 't2_space_dark-fluid_sag_p2_ns-t2prep';
 
-	end
+		case 'SWI'
 
+			series_description_keyword = 'greME7_p31_256_Iso1mm';
+
+		case 'DTI'
+
+			series_description_keyword = 'DIFFUSION';
+
+		case 'ASL'
+
+			series_description_keyword = 'asl_3d_tra_p2_iso_3mm_highres';
+
+		case 'CVR_fmap'
+
+			series_description_keyword = 'gre_field_mapping 3.8mm';
+
+		case 'CVR'
+
+			series_description_keyword = 'CVR_ep2d_bold 3.8mm TR1500 adaptive';
+
+		case 'MP2RAGE'
+
+			series_description_keyword = 't1_mp2rage_sag_0.8x0.8x2';
+
+		case 'DCE'
+
+			series_description_keyword = 't1_vibe_sag_DCE_2mm XL FOV 40s temporal res';
+
+	end
 
 	fprintf ('%s : Start organising %s DICOM directory. We assume all %s sequences have keyword of ''%s''.\n', mfilename, modality, modality, series_description_keyword);
 
-	curr_dcmCollection_subtable = dcm_coll(find(contains(dcm_coll.SeriesDescription,series_description_keyword,'IgnoreCase',true)),:);
-	
-	fprintf ('%s : %d entries found in the dicomCollection table with keyword ''%s''. Series description includes: \n', mfilename, size (curr_dcmCollection_subtable,1), series_description_keyword);
+	curr_dcmCollection_subtable = dcm_coll(find(contains(dcm_coll.SeriesDescription, series_description_keyword, 'IgnoreCase', true)),:);
 
-	for i = 1 : size(curr_dcmCollection_subtable,1)
-		fprintf ('  >>> %s\n', curr_dcmCollection_subtable.SeriesDescription{i});
+	curr_dcmCollection_SeriesDescUniqueVal = unique(curr_dcmCollection_subtable.SeriesDescription);
+	
+	fprintf ('%s : %d entries found in the dicomCollection table with keyword ''%s''. Unique SeriesDescription values include: \n', mfilename, length(curr_dcmCollection_SeriesDescUniqueVal), series_description_keyword);
+
+	for i = 1 : length(curr_dcmCollection_SeriesDescUniqueVal)
+		fprintf ('  >>> %s\n', curr_dcmCollection_SeriesDescUniqueVal(i));
 	end
 
-	for i = 1 : size(curr_dcmCollection_subtable,1)
+	for i = 1 : size(curr_dcmCollection_SeriesDescUniqueVal,1)
 
-		fprintf ('%s : Now dealing with SeriesDescription = ''%s''.\n', mfilename, curr_dcmCollection_subtable.SeriesDescription{i});
+		fprintf ('%s : Now dealing with SeriesDescription = ''%s''.\n', mfilename, curr_dcmCollection_SeriesDescUniqueVal(i));
+		% fprintf ('%s : Note that since %s is 4D image (i.e., for each unique SeriesDescription, there''re multiple 3D images), we use SeriesInstanceUID as sub-folder names to separate these 3D images.\n', mfilename, modality);
 
-		DICOM_bmp_path = fullfile (DICOM_parent_folder, 'DICOM_bmp', strrep(curr_dcmCollection_subtable.SeriesDescription{i},' ','_'));
+		curr_dcmCollection_subtable_currSeriesDescSubTable = curr_dcmCollection_subtable(find(strcmp(curr_dcmCollection_subtable.SeriesDescription, curr_dcmCollection_SeriesDescUniqueVal(i))),:);
 
-		if ~isfolder (DICOM_bmp_path)
+		for j = 1 : size (curr_dcmCollection_subtable_currSeriesDescSubTable,1)
 
-			fprintf ('%s : %s folder doesn''t exist. Creating ... ', mfilename, DICOM_bmp_path);
-			mkdir (DICOM_bmp_path);
-			fprintf ('DONE!\n');
+			fprintf ('%s : Current SeriesDescription = %s; SeriesInstanceUID = %s.\n', mfilename, curr_dcmCollection_SeriesDescUniqueVal(i), curr_dcmCollection_subtable_currSeriesDescSubTable.SeriesInstanceUID{j});
 
-		else
+			DICOM_bmp_path = fullfile (DICOM_parent_folder, 'DICOM_bmp', strrep(curr_dcmCollection_SeriesDescUniqueVal(i),' ','_'), curr_dcmCollection_subtable_currSeriesDescSubTable.SeriesInstanceUID{j});
 
-			fprintf ('%s : %s folder exists.\n', mfilename, DICOM_bmp_path);
+			if ~isfolder (DICOM_bmp_path)
 
-		end
+				fprintf ('%s : %s folder doesn''t exist. Creating ... ', mfilename, DICOM_bmp_path);
+				mkdir (DICOM_bmp_path);
+				fprintf ('DONE!\n');
 
-		filenames_for_curr_seq = strrep(curr_dcmCollection_subtable.Filenames{i}, str_toBeReplaced, str_replaceTo); 
+			else
 
-		fprintf ('%s : Copying DICOM files for %s (N = %d) to %s ... ', mfilename, curr_dcmCollection_subtable.SeriesDescription{i}, size (filenames_for_curr_seq,1), DICOM_bmp_path);
+				fprintf ('%s : %s folder exists.\n', mfilename, DICOM_bmp_path);
 
-		for j = 1 : size (filenames_for_curr_seq,1)
-
-			[status,message,messageId] = copyfile (filenames_for_curr_seq(j,1), DICOM_bmp_path);
-
-			if ~status
-				fprintf ('%s : [WARNING] : %s\n', mfilename, message);
 			end
 
+			filenames_for_curr_seq = strrep(curr_dcmCollection_subtable_currSeriesDescSubTable.Filenames{j}, str_toBeReplaced, str_replaceTo); 
+
+			fprintf ('%s : Copying ''SeriesDescription=%s'' DICOM files ''SeriesInstanceUID=%s'' (N = %d) ... ', mfilename, curr_dcmCollection_SeriesDescUniqueVal(i), curr_dcmCollection_subtable_currSeriesDescSubTable.SeriesInstanceUID{j}, size (filenames_for_curr_seq,1));
+
+			for k = 1 : size (filenames_for_curr_seq,1)
+
+				[status,message,messageId] = copyfile (filenames_for_curr_seq(k,1), DICOM_bmp_path);
+
+				if ~status
+					fprintf ('%s : [WARNING] : %s\n', mfilename, message);
+				end
+
+			end
+
+			fprintf ('DONE!\n');
+
 		end
 
-		fprintf ('DONE!\n');
-
 	end
-
-end
-
-
-function orgniseDICOMdir_dwi (dcm_coll, DICOM_parent_folder, str_toBeReplaced, str_replaceTo)
-
-	fprintf ('%s : Start organising dMRI DICOM directory. We assume all dMRI sequences have keyword of ''DIFFUSION''.\n', mfilename);
-
-	dwi_dcmCollection_subtable = dcm_coll(find(contains(dcm_coll.SeriesDescription,'DIFFUSION','IgnoreCase',true)),:);
-
-	dwi_dcmCollection_SeriesDescUniqueVal = unique(dwi_dcmCollection_subtable.SeriesDescription);
-	
-	fprintf ('%s : %d entries found in the dicomCollection table with keyword ''DIFFUSION''. Series description includes: \n', mfilename, length(dwi_dcmCollection_SeriesDescUniqueVal));
-
-	for i = 1 : length(dwi_dcmCollection_SeriesDescUniqueVal)
-		fprintf ('  >>> %s\n', dwi_dcmCollection_SeriesDescUniqueVal(i));
-	end
-
-	% for i = 1 : size(dwi_dcmCollection_subtable,1)
-
-	% 	fprintf ('%s : Now dealing with SeriesDescription = ''%s''.\n', mfilename, dwi_dcmCollection_subtable.SeriesDescription{i});
-
-	% 	DICOM_bmp_path = fullfile (DICOM_parent_folder, 'DICOM_bmp', strrep(dwi_dcmCollection_subtable.SeriesDescription{i},' ','_'));
-
-	% 	if ~isfolder (DICOM_bmp_path)
-
-	% 		fprintf ('%s : %s folder doesn''t exist. Creating ... ', mfilename, DICOM_bmp_path);
-	% 		mkdir (DICOM_bmp_path);
-	% 		fprintf ('DONE!\n');
-
-	% 	else
-
-	% 		fprintf ('%s : %s folder exists.\n', mfilename, DICOM_bmp_path);
-
-	% 	end
-
-	% 	filenames_for_curr_seq = strrep(dwi_dcmCollection_subtable.Filenames{i}, str_toBeReplaced, str_replaceTo); 
-
-	% 	fprintf ('%s : Copying DICOM files for %s (N = %d) to %s ... ', mfilename, dwi_dcmCollection_subtable.SeriesDescription{i}, size (filenames_for_curr_seq,1), DICOM_bmp_path);
-
-	% 	for j = 1 : size (filenames_for_curr_seq,1)
-
-	% 		[status,message,messageId] = copyfile (filenames_for_curr_seq(j,1), DICOM_bmp_path);
-
-	% 		if ~status
-	% 			fprintf ('%s : [WARNING] : %s\n', mfilename, message);
-	% 		end
-
-	% 	end
-
-	% 	fprintf ('DONE!\n');
-
-	% end
 
 end
