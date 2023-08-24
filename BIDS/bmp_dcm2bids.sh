990692b83358b2e6e5eee426f110432ad2171f38
Jiyang Jiang <54608573+JiyangJiang@users.noreply.github.com>
2023-08-24 13:49:35 +1000

    Update bmp_dcm2bids.sh

--- BIDS/bmp_dcm2bids.sh
+++ BIDS/bmp_dcm2bids.sh
@@ -215,10 +215,10 @@ case $is_first_run in
 
 				echo -e "$(bmp_convention.sh --text_normal)[$(date)] : $(basename $0) : Running dcm2bids_helper to convert DICOM of the current subject (ID = $curr_subjID) to NIFTI and json, so that configuration file can be prepared.$(bmp_shellColour.sh --reset)"
 
-				dcm2bids_helper --dicom_dir   $BIDS_directory/sourcedata/$curr_subjID \
+				dcm2bids_helper --dicom_dir $BIDS_directory/sourcedata/$curr_subjID \
 								--nest \
 								--log_level DEBUG \
-								--firce \
+								--force \
 								> $BIDS_directory/tmp_dcm2bids/dcm2bids_helper.debug_log
 
 				echo -e "$(bmp_convention.sh --text_normal)[$(date)] : $(basename $0) : Investigate json files in $(bmp_convention.sh --text_path)$BIDS_directory/tmp_dcm2bids/sourcedata/$curr_subjID$(bmp_convention.sh --text_normal) to create the configuration file.$(bmp_shellColour.sh --reset)"
