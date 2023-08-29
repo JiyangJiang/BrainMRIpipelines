628488b8c9c6bcded92b45578f85ef4de7159435
Jiyang Jiang <54608573+JiyangJiang@users.noreply.github.com>
2023-08-29 13:46:09 +1000

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,8 +28,9 @@ intersphinx_disabled_domains = ['std']
 templates_path = ['_templates']
 
 # -- Options for HTML output
-
+import sphinx_rtd_theme
 html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
