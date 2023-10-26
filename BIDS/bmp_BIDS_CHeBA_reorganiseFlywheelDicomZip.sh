cca5f47272918b277c005309b97f0e7ec71fab45
Jiyang Jiang <54608573+JiyangJiang@users.noreply.github.com>
2023-10-26 22:02:18 +1100

    Update bmp_BIDS_CHeBA_reorganiseFlywheelDicomZip.sh

--- BIDS/bmp_BIDS_CHeBA_reorganiseFlywheelDicomZip.sh
+++ BIDS/bmp_BIDS_CHeBA_reorganiseFlywheelDicomZip.sh
@@ -10,85 +10,201 @@ DICOM_zip=$1
 BIDS_dir=$2
 subject_ID=$3
 
+deal_with_3D(){
+
+	DICOM_zip=$1
+	BIDS_dir=$2
+	subject_ID=$3
+	zip_path_kword1=$4
+	zip_path_kword2=$5
+	modality_name=$6
+
+	DICOM="flywheel$(unzip -l $DICOM_zip | grep "$zip_path_kword1" | grep "$zip_path_kword2" | grep ".dcm" | awk -F'flywheel' '{print $NF}')"
+
+	echo $DICOM
+
+	# # curr_rand_str=$(LC_CTYPE=C tr -dc 'a-zA-Z0-9' </dev/urandom | head -c 13 ; echo '')
+	# curr_rand_str=$(head -c 5 /dev/random | openssl base64 | sed 's/\///g')
+
+	# echo "unzip -o -j $DICOM_zip -d $BIDS_dir/sourcedata/$subject_ID/$modality_name $(echo $DICOM | sed 's/ /\\ /g')" > $BMP_TMP_PATH/bmp_${curr_rand_str}.sh
+
+	# mkdir -p $BIDS_dir/sourcedata/$subject_ID/$modality_name
+
+	# bash $BMP_TMP_PATH/bmp_${curr_rand_str}.sh && rm -f $BMP_TMP_PATH/bmp_${curr_rand_str}.sh
+
+	# mv "$BIDS_dir/sourcedata/$subject_ID/$modality_name/$(echo $DICOM | awk -F/ '{print $NF}')" "$BIDS_dir/sourcedata/$subject_ID/$modality_name/${modality_name}.dcm"
+}
+
+deal_with_4D(){
+
+	DICOM_zip=$1
+	BIDS_dir=$2
+	subject_ID=$3
+	zip_path_kword1=$4
+	zip_path_kword2=$5
+	modality_name=$6
+
+	DICOM="flywheel$(unzip -l $DICOM_zip | grep "$zip_path_kword1" | grep "$zip_path_kword2" | grep ".dicom.zip" | awk -F'flywheel' '{print $NF}')"
+
+	echo $DICOM
+
+	# # curr_rand_str=$(LC_CTYPE=C tr -dc 'a-zA-Z0-9' </dev/urandom | head -c 13 ; echo '')
+	# curr_rand_str=$(head -c 5 /dev/random | openssl base64 | sed 's/\///g')
+
+	# echo "unzip -o -j $DICOM_zip -d $BIDS_dir/sourcedata/$subject_ID/$modality_name $(echo $DICOM | sed 's/ /\\ /g')" > $BMP_TMP_PATH/bmp_${curr_rand_str}.sh
+
+	# mkdir -p $BIDS_dir/sourcedata/$subject_ID/$modality_name
+
+	# bash $BMP_TMP_PATH/bmp_${curr_rand_str}.sh && rm -f $BMP_TMP_PATH/bmp_${curr_rand_str}.sh
+
+	# mv "$BIDS_dir/sourcedata/$subject_ID/$modality_name/$(echo $DICOM | awk -F/ '{print $NF}')" "$BIDS_dir/sourcedata/$subject_ID/$modality_name/${modality_name}.dicom.zip"
+
+	# unzip -o -d $BIDS_dir/sourcedata/$subject_ID/$modality_name "$BIDS_dir/sourcedata/$subject_ID/$modality_name/${modality_name}.dicom.zip" && rm -f "$BIDS_dir/sourcedata/$subject_ID/$modality_name/${modality_name}.dicom.zip"
+}
 
 # MEMPRAGE RMS
-DICOM_MEMPRAGE_RMS="flywheel$(unzip -l $DICOM_zip | grep "/T1_MEMPRAGE" | grep "RMS/" | grep ".dcm" | awk -F'flywheel' '{print $NF}')"
-curr_rand_str=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 13 ; echo '')
-echo "unzip -o -j $DICOM_zip -d $BIDS_dir/sourcedata/$subject_ID/MEMPRAGE_RMS $(echo $DICOM_MEMPRAGE_RMS | sed 's/ /\\ /g')" > $BMP_TMP_PATH/bmp_${curr_rand_str}.sh
-mkdir -p $BIDS_dir/sourcedata/$subject_ID/MEMPRAGE_RMS
-bash $BMP_TMP_PATH/bmp_${curr_rand_str}.sh
-rm -f $BMP_TMP_PATH/bmp_${curr_rand_str}.sh
-mv "$BIDS_dir/sourcedata/$subject_ID/MEMPRAGE_RMS/$(echo $DICOM_MEMPRAGE_RMS | awk -F/ '{print $NF}')" $BIDS_dir/sourcedata/$subject_ID/MEMPRAGE_RMS/MEMPRAGE_RMS.dcm
+# ++++++++++++++++++++++++++++++
+zip_path_kword1="/ABCD_T1w_MPR"
+zip_path_kword2="RMS/"
+modality_name=MEMPRAGE_RMS
+
+deal_with_3D $1 $2 $3 \
+				$zip_path_kword1 $zip_path_kword2 $modality_name
+
+# MEMPRAGE individual echoes
+# ++++++++++++++++++++++++++++++
+zip_path_kword1="/ABCD_T1w_MPR"
+zip_path_kword2="_BW740/"
+modality_name=MEMPRAGE_RMS
+
+deal_with_4D $1 $2 $3 \
+				$zip_path_kword1 $zip_path_kword2 $modality_name
 
 # FLAIR
-DICOM_FLAIR="flywheel$(unzip -l $DICOM_zip | grep "/t2_space_dark-fluid_sag_p2_ns-t2prep" | grep ".dcm" | awk -F'flywheel' '{print $NF}')"
-curr_rand_str=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 13 ; echo '')
-echo "unzip -o -j $DICOM_zip -d $BIDS_dir/sourcedata/$subject_ID/FLAIR $(echo $DICOM_FLAIR | sed 's/ /\\ /g')" > $BMP_TMP_PATH/bmp_${curr_rand_str}.sh
-mkdir -p $BIDS_dir/sourcedata/$subject_ID/FLAIR
-bash $BMP_TMP_PATH/bmp_${curr_rand_str}.sh
-rm -f $BMP_TMP_PATH/bmp_${curr_rand_str}.sh
-mv "$BIDS_dir/sourcedata/$subject_ID/FLAIR/$(echo $DICOM_FLAIR | awk -F/ '{print $NF}')" $BIDS_dir/sourcedata/$subject_ID/FLAIR/FLAIR.dcm
+# ++++++++++++++++++++++++++++++
+zip_path_kword1="/t2_space"
+zip_path_kword2="_DF"
+modality_name=FLAIR
+deal_with_3D $1 $2 $3 \
+				$zip_path_kword1 $zip_path_kword2 $modality_name
 
 # T2w
-DICOM_T2W="flywheel$(unzip -l $DICOM_zip | grep -v "setter" | grep "/ABCD_T2w_SPC_" | grep ".dcm" | awk -F'flywheel' '{print $NF}')"
-curr_rand_str=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 13 ; echo '')
-echo "unzip -o -j $DICOM_zip -d $BIDS_dir/sourcedata/$subject_ID/T2W $(echo $DICOM_T2W | sed 's/ /\\ /g')" > $BMP_TMP_PATH/bmp_${curr_rand_str}.sh
-mkdir -p $BIDS_dir/sourcedata/$subject_ID/T2W
-bash $BMP_TMP_PATH/bmp_${curr_rand_str}.sh
-rm -f $BMP_TMP_PATH/bmp_${curr_rand_str}.sh
-mv "$BIDS_dir/sourcedata/$subject_ID/T2W/$(echo $DICOM_T2W | awk -F/ '{print $NF}')" $BIDS_dir/sourcedata/$subject_ID/T2W/T2W.dcm
+# ++++++++++++++++++++++++++++++
+zip_path_kword1="/ABCD_T2w_SPC_"
+zip_path_kword2="BW744/"
+modality_name=T2w
+deal_with_3D $1 $2 $3 \
+				$zip_path_kword1 $zip_path_kword2 $modality_name
 
 # DWI
-DICOM_DWIap1="flywheel$(unzip -l $DICOM_zip | grep "/AP_BLOCK_1_DIFFUSION_30DIR/" | grep ".dicom.zip" | awk -F'flywheel' '{print $NF}')"
-DICOM_DWIap2="flywheel$(unzip -l $DICOM_zip | grep "/AP_BLOCK_2_DIFFUSION_30DIR/" | grep ".dicom.zip" | awk -F'flywheel' '{print $NF}')"
-DICOM_DWIpa1="flywheel$(unzip -l $DICOM_zip | grep "/PA_BLOCK_1_DIFFUSION_30DIR/" | grep ".dicom.zip" | awk -F'flywheel' '{print $NF}')"
-DICOM_DWIpa2="flywheel$(unzip -l $DICOM_zip | grep "/PA_BLOCK_2_DIFFUSION_30DIR/" | grep ".dicom.zip" | awk -F'flywheel' '{print $NF}')"
-DICOM_DWIfmapAp="flywheel$(unzip -l $DICOM_zip | grep "/AP_FMAP_for DIFFUSION/" | grep ".dicom.zip" | awk -F"flywheel" '{print $NF}')"
-DICOM_DWIfmapPa="flywheel$(unzip -l $DICOM_zip | grep "/PA_FMAP_for DIFFUSION/" | grep ".dicom.zip" | awk -F"flywheel" '{print $NF}')"
+# ++++++++++++++++++++++++++++++
+zip_path_kword1="/AP_BLOCK_1"
+zip_path_kword2="_DIFFUSION_30DIR/"
+modality_name=DWI_AP_1
+deal_with_4D $1 $2 $3 \
+				$zip_path_kword1 $zip_path_kword2 $modality_name
 
-for i in "$DICOM_DWIap1" "$DICOM_DWIap2" "$DICOM_DWIpa1" "$DICOM_DWIpa2" "$DICOM_DWIfmapAp" "$DICOM_DWIfmapPa"
-do
-	curr_rand_str=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 13 ; echo '')
-	echo "unzip -o -j $DICOM_zip -d $BIDS_dir/sourcedata/$subject_ID $(echo $i | sed 's/ /\\ /g')" > $BMP_TMP_PATH/bmp_${curr_rand_str}.sh
-	bash $BMP_TMP_PATH/bmp_${curr_rand_str}.sh
-	rm -f $BMP_TMP_PATH/bmp_${curr_rand_str}.sh
-done
+zip_path_kword1="/AP_BLOCK_2"
+zip_path_kword2="_DIFFUSION_30DIR/"
+modality_name=DWI_AP_2
+deal_with_4D $1 $2 $3 \
+				$zip_path_kword1 $zip_path_kword2 $modality_name
 
-unzip -o "$BIDS_dir/sourcedata/$subject_ID/$(echo $DICOM_DWIap1 | awk -F/ '{print $NF}')" -d $BIDS_dir/sourcedata/$subject_ID/DWI_AP1
-unzip -o "$BIDS_dir/sourcedata/$subject_ID/$(echo $DICOM_DWIap2 | awk -F/ '{print $NF}')" -d $BIDS_dir/sourcedata/$subject_ID/DWI_AP2
-unzip -o "$BIDS_dir/sourcedata/$subject_ID/$(echo $DICOM_DWIpa1 | awk -F/ '{print $NF}')" -d $BIDS_dir/sourcedata/$subject_ID/DWI_PA1
-unzip -o "$BIDS_dir/sourcedata/$subject_ID/$(echo $DICOM_DWIpa2 | awk -F/ '{print $NF}')" -d $BIDS_dir/sourcedata/$subject_ID/DWI_PA2
-unzip -o "$BIDS_dir/sourcedata/$subject_ID/$(echo $DICOM_DWIfmapAp | awk -F/ '{print $NF}')" -d $BIDS_dir/sourcedata/$subject_ID/DWI_FMAP_AP
-unzip -o "$BIDS_dir/sourcedata/$subject_ID/$(echo $DICOM_DWIfmapPa | awk -F/ '{print $NF}')" -d $BIDS_dir/sourcedata/$subject_ID/DWI_FMAP_PA
+zip_path_kword1="/PA_BLOCK_1"
+zip_path_kword2="_DIFFUSION_30DIR/"
+modality_name=DWI_PA_1
+deal_with_4D $1 $2 $3 \
+				$zip_path_kword1 $zip_path_kword2 $modality_name
 
-rm -f "$BIDS_dir/sourcedata/$subject_ID/$(echo $DICOM_DWIap1 | awk -F/ '{print $NF}')"
-rm -f "$BIDS_dir/sourcedata/$subject_ID/$(echo $DICOM_DWIap2 | awk -F/ '{print $NF}')"
-rm -f "$BIDS_dir/sourcedata/$subject_ID/$(echo $DICOM_DWIpa1 | awk -F/ '{print $NF}')"
-rm -f "$BIDS_dir/sourcedata/$subject_ID/$(echo $DICOM_DWIpa2 | awk -F/ '{print $NF}')"
-rm -f "$BIDS_dir/sourcedata/$subject_ID/$(echo $DICOM_DWIfmapAp | awk -F/ '{print $NF}')"
-rm -f "$BIDS_dir/sourcedata/$subject_ID/$(echo $DICOM_DWIfmapPa | awk -F/ '{print $NF}')"
+zip_path_kword1="/PA_BLOCK_2"
+zip_path_kword2="_DIFFUSION_30DIR/"
+modality_name=DWI_PA_2
+deal_with_4D $1 $2 $3 \
+				$zip_path_kword1 $zip_path_kword2 $modality_name
+
+zip_path_kword1="/AP_FMAP"
+zip_path_kword2="DIFFUSION/"
+modality_name=DWI_B0_AP
+deal_with_4D $1 $2 $3 \
+				$zip_path_kword1 $zip_path_kword2 $modality_name
+
+zip_path_kword1="/PA_FMAP"
+zip_path_kword2="DIFFUSION/"
+modality_name=DWI_B0_PA
+deal_with_4D $1 $2 $3 \
+				$zip_path_kword1 $zip_path_kword2 $modality_name
+
 
 # SWI-QSM
-DICOM_SWI_QSM="flywheel$(unzip -l $DICOM_zip | grep "/greME9_p31_256_Iso1mm_new_Qsm/" | grep ".dcm" | awk -F'flywheel' '{print $NF}')"
-curr_rand_str=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 13 ; echo '')
-echo "unzip -o -j $DICOM_zip -d $BIDS_dir/sourcedata/$subject_ID/SWI_QSM $(echo $DICOM_SWI_QSM | sed 's/ /\\ /g')" > $BMP_TMP_PATH/bmp_${curr_rand_str}.sh
-mkdir -p $BIDS_dir/sourcedata/$subject_ID/SWI_QSM
-bash $BMP_TMP_PATH/bmp_${curr_rand_str}.sh
-rm -f $BMP_TMP_PATH/bmp_${curr_rand_str}.sh
-mv "$BIDS_dir/sourcedata/$subject_ID/SWI_QSM/$(echo $DICOM_SWI_QSM | awk -F/ '{print $NF}')" $BIDS_dir/sourcedata/$subject_ID/SWI_QSM/SWI_QSM.dcm
+# ++++++++++++++++++++++++++++++
+zip_path_kword1="/greME9_p31"
+zip_path_kword2="_Qsm/"
+modality_name=SWI_QSM
+deal_with_3D $1 $2 $3 \
+				$zip_path_kword1 $zip_path_kword2 $modality_name
 
 # SWI-SWI
-DICOM_SWI_SWI="flywheel$(unzip -l $DICOM_zip | grep "/greME9_p31_256_Iso1mm_new_SWI_Combined/" | grep ".dcm" | awk -F'flywheel' '{print $NF}')"
-curr_rand_str=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 13 ; echo '')
-echo "unzip -o -j $DICOM_zip -d $BIDS_dir/sourcedata/$subject_ID/SWI_SWI $(echo $DICOM_SWI_SWI | sed 's/ /\\ /g')" > $BMP_TMP_PATH/bmp_${curr_rand_str}.sh
-mkdir -p $BIDS_dir/sourcedata/$subject_ID/SWI_SWI
-bash $BMP_TMP_PATH/bmp_${curr_rand_str}.sh
-rm -f $BMP_TMP_PATH/bmp_${curr_rand_str}.sh
-mv "$BIDS_dir/sourcedata/$subject_ID/SWI_SWI/$(echo $DICOM_SWI_SWI | awk -F/ '{print $NF}')" $BIDS_dir/sourcedata/$subject_ID/SWI_SWI/SWI_SWI.dcm
+# ++++++++++++++++++++++++++++++
+zip_path_kword1="/greME9_p31"
+zip_path_kword2="SWI_Combined/"
+modality_name=SWI_SWI
+deal_with_3D $1 $2 $3 \
+				$zip_path_kword1 $zip_path_kword2 $modality_name
 
 # SWI-mIP
-DICOM_SWI_MIP="flywheel$(unzip -l $DICOM_zip | grep "/greME9_p31_256_Iso1mm_new_SWI_mIP_Combined/" | grep ".dcm" | awk -F'flywheel' '{print $NF}')"
+# ++++++++++++++++++++++++++++++
+zip_path_kword1="/greME9_p31"
+zip_path_kword2="SWI_mIP_Combined/"
+modality_name=SWI_mIP
+deal_with_3D $1 $2 $3 \
+				$zip_path_kword1 $zip_path_kword2 $modality_name
 
 # SWI-magnitude
-DICOM_SWI_MAG="flywheel$(unzip -l $DICOM_zip | grep "/greME9_p31_256_Iso1mm_new_Mag/" | 
+# ++++++++++++++++++++++++++++++
+zip_path_kword1="/greME9_p31"
+zip_path_kword2="_Mag/"
+modality_name=SWI_mag
+deal_with_4D $1 $2 $3 \
+				$zip_path_kword1 $zip_path_kword2 $modality_name
 
+# SWI-phase
+# ++++++++++++++++++++++++++++++
+zip_path_kword1="/greME9_p31"
+zip_path_kword2="_Pha/"
+modality_name=SWI_pha
+deal_with_4D $1 $2 $3 \
+				$zip_path_kword1 $zip_path_kword2 $modality_name
+
+# ASL-relCBF
+# ++++++++++++++++++++++++++++++
+zip_path_kword1="/relCBF/"
+zip_path_kword2="/relCBF/"
+modality_name=ASL_relCBF
+deal_with_3D $1 $2 $3 \
+				$zip_path_kword1 $zip_path_kword2 $modality_name
+
+# ASL-BAT
+# ++++++++++++++++++++++++++++++
+zip_path_kword1="/BAT/"
+zip_path_kword2="/BAT/"
+modality_name=ASL_BAT
+deal_with_3D $1 $2 $3 \
+				$zip_path_kword1 $zip_path_kword2 $modality_name
+
+# ASL-PWI
+# ++++++++++++++++++++++++++++++
+zip_path_kword1="/Perfusion_Weighted/"
+zip_path_kword2="/Perfusion_Weighted/"
+modality_name=ASL_PWI
+deal_with_4D $1 $2 $3 \
+				$zip_path_kword1 $zip_path_kword2 $modality_name
+
+# ASL-ASL
+# ++++++++++++++++++++++++++++++
+zip_path_kword1="/mTI16"
+zip_path_kword2="pcasl"
+modality_name=ASL_ASL
+deal_with_4D $1 $2 $3 \
+				$zip_path_kword1 $zip_path_kword2 $modality_name
+
+				
