b6e3f0a77f45f51736fe1cd61d99191a451cbd46
Jiyang Jiang <54608573+JiyangJiang@users.noreply.github.com>
2023-10-27 20:18:01 +1100

    upd bids

--- BIDS/bmp_BIDS_CHeBA_reorganiseFlywheelDicomZip.sh
+++ BIDS/bmp_BIDS_CHeBA_reorganiseFlywheelDicomZip.sh
@@ -5,21 +5,17 @@
 #
 # Written by Dr. Jiyang Jiang (18th September, 2023)
 #
-
-DICOM_zip=$1
-BIDS_dir=$2
-subject_ID=$3
+# Updated 27th October, 2023
 
 deal_with_3D(){
 
 	DICOM_zip=$1
 	BIDS_dir=$2
 	subject_ID=$3
-	zip_path_kword1=$4
-	zip_path_kword2=$5
-	modality_name=$6
+	zip_path_kword=$4
+	modality_name=$5
 
-	DICOM="flywheel$(unzip -l $DICOM_zip | grep "$zip_path_kword1" | grep "$zip_path_kword2" | grep ".dcm" | awk -F'flywheel' '{print $NF}')"
+	DICOM="flywheel$(unzip -l $DICOM_zip | grep "$zip_path_kword" | grep ".dcm" | awk -F'flywheel' '{print $NF}')"
 
 	echo $DICOM
 
@@ -40,11 +36,10 @@ deal_with_4D(){
 	DICOM_zip=$1
 	BIDS_dir=$2
 	subject_ID=$3
-	zip_path_kword1=$4
-	zip_path_kword2=$5
-	modality_name=$6
+	zip_path_kword=$4
+	modality_name=$5
 
-	DICOM="flywheel$(unzip -l $DICOM_zip | grep "$zip_path_kword1" | grep "$zip_path_kword2" | grep ".dicom.zip" | awk -F'flywheel' '{print $NF}')"
+	DICOM="flywheel$(unzip -l $DICOM_zip | grep "$zip_path_kword" | grep ".dicom.zip" | awk -F'flywheel' '{print $NF}')"
 
 	echo $DICOM
 
@@ -62,149 +57,31 @@ deal_with_4D(){
 	# unzip -o -d $BIDS_dir/sourcedata/$subject_ID/$modality_name "$BIDS_dir/sourcedata/$subject_ID/$modality_name/${modality_name}.dicom.zip" && rm -f "$BIDS_dir/sourcedata/$subject_ID/$modality_name/${modality_name}.dicom.zip"
 }
 
-# MEMPRAGE RMS
-# ++++++++++++++++++++++++++++++
-zip_path_kword1="/ABCD_T1w_MPR"
-zip_path_kword2="RMS/"
-modality_name=MEMPRAGE_RMS
 
-deal_with_3D $1 $2 $3 \
-				$zip_path_kword1 $zip_path_kword2 $modality_name
-
-# MEMPRAGE individual echoes
-# ++++++++++++++++++++++++++++++
-zip_path_kword1="/ABCD_T1w_MPR"
-zip_path_kword2="_BW740/"
-modality_name=MEMPRAGE_RMS
-
-deal_with_4D $1 $2 $3 \
-				$zip_path_kword1 $zip_path_kword2 $modality_name
-
-# FLAIR
-# ++++++++++++++++++++++++++++++
-zip_path_kword1="/t2_space"
-zip_path_kword2="_DF"
-modality_name=FLAIR
-deal_with_3D $1 $2 $3 \
-				$zip_path_kword1 $zip_path_kword2 $modality_name
-
-# T2w
-# ++++++++++++++++++++++++++++++
-zip_path_kword1="/ABCD_T2w_SPC_"
-zip_path_kword2="BW744/"
-modality_name=T2w
-deal_with_3D $1 $2 $3 \
-				$zip_path_kword1 $zip_path_kword2 $modality_name
-
-# DWI
-# ++++++++++++++++++++++++++++++
-zip_path_kword1="/AP_BLOCK_1"
-zip_path_kword2="_DIFFUSION_30DIR/"
-modality_name=DWI_AP_1
-deal_with_4D $1 $2 $3 \
-				$zip_path_kword1 $zip_path_kword2 $modality_name
-
-zip_path_kword1="/AP_BLOCK_2"
-zip_path_kword2="_DIFFUSION_30DIR/"
-modality_name=DWI_AP_2
-deal_with_4D $1 $2 $3 \
-				$zip_path_kword1 $zip_path_kword2 $modality_name
-
-zip_path_kword1="/PA_BLOCK_1"
-zip_path_kword2="_DIFFUSION_30DIR/"
-modality_name=DWI_PA_1
-deal_with_4D $1 $2 $3 \
-				$zip_path_kword1 $zip_path_kword2 $modality_name
-
-zip_path_kword1="/PA_BLOCK_2"
-zip_path_kword2="_DIFFUSION_30DIR/"
-modality_name=DWI_PA_2
-deal_with_4D $1 $2 $3 \
-				$zip_path_kword1 $zip_path_kword2 $modality_name
-
-zip_path_kword1="/AP_FMAP"
-zip_path_kword2="DIFFUSION/"
-modality_name=DWI_B0_AP
-deal_with_4D $1 $2 $3 \
-				$zip_path_kword1 $zip_path_kword2 $modality_name
-
-zip_path_kword1="/PA_FMAP"
-zip_path_kword2="DIFFUSION/"
-modality_name=DWI_B0_PA
-deal_with_4D $1 $2 $3 \
-				$zip_path_kword1 $zip_path_kword2 $modality_name
+DICOM_zip=$1
+BIDS_dir=$2
+subject_ID=$3
 
 
-# SWI-QSM
-# ++++++++++++++++++++++++++++++
-zip_path_kword1="/greME9_p31"
-zip_path_kword2="_Qsm/"
-modality_name=SWI_QSM
-deal_with_3D $1 $2 $3 \
-				$zip_path_kword1 $zip_path_kword2 $modality_name
 
-# SWI-SWI
-# ++++++++++++++++++++++++++++++
-zip_path_kword1="/greME9_p31"
-zip_path_kword2="SWI_Combined/"
-modality_name=SWI_SWI
-deal_with_3D $1 $2 $3 \
-				$zip_path_kword1 $zip_path_kword2 $modality_name
+deal_with_3D $DICOM_zip $BIDS_dir $subject_ID "/ABCD_T1w_MPR_vNav_BW740 RMS/"							MEMPRAGE_RMS
+deal_with_4D $DICOM_zip $BIDS_dir $subject_ID "/ABCD_T1w_MPR_vNav_BW740/"								MEMPRAGE_echoes
+deal_with_3D $DICOM_zip $BIDS_dir $subject_ID "/t2_space_DF_BW651/"										FLAIR
+deal_with_3D $DICOM_zip $BIDS_dir $subject_ID "/ABCD_T2w_SPC_ vNav Iso0.8mm BW744/"						T2w
+deal_with_4D $DICOM_zip $BIDS_dir $subject_ID "/AP_BLOCK_1_DIFFUSION_30DIR/"							DWI_AP_1
+deal_with_4D $DICOM_zip $BIDS_dir $subject_ID "/AP_BLOCK_2_DIFFUSION_30DIR/"							DWI_AP_2
+deal_with_4D $DICOM_zip $BIDS_dir $subject_ID "/PA_BLOCK_1_DIFFUSION_30DIR/"							DWI_PA_1
+deal_with_4D $DICOM_zip $BIDS_dir $subject_ID "/PA_BLOCK_2_DIFFUSION_30DIR/"							DWI_PA_2
+deal_with_4D $DICOM_zip $BIDS_dir $subject_ID "/AP_FMAP_for DIFFUSION/"									DWI_B0_AP
+deal_with_4D $DICOM_zip $BIDS_dir $subject_ID "/PA_FMAP_for DIFFUSION/"									DWI_B0_PA
+deal_with_3D $DICOM_zip $BIDS_dir $subject_ID "/greME9_p31_256_Iso1mm_Qsm/"								SWI_QSM
+deal_with_3D $DICOM_zip $BIDS_dir $subject_ID "/greME9_p31_256_Iso1mm_SWI_Combined/"					SWI_SWI
+deal_with_3D $DICOM_zip $BIDS_dir $subject_ID "/greME9_p31_256_Iso1mm_SWI_mIP_Combined/"				SWI_mIP
+deal_with_4D $DICOM_zip $BIDS_dir $subject_ID "/greME9_p31_256_Iso1mm_Mag/"								SWI_mag
+deal_with_4D $DICOM_zip $BIDS_dir $subject_ID "/greME9_p31_256_Iso1mm_Pha/"								SWI_pha
+deal_with_3D $DICOM_zip $BIDS_dir $subject_ID "/relCBF/"												ASL_relCBF
+deal_with_3D $DICOM_zip $BIDS_dir $subject_ID "/BAT/"													ASL_BAT
+deal_with_4D $DICOM_zip $BIDS_dir $subject_ID "/Perfusion_Weighted/"									ASL_PWI
+deal_with_4D $DICOM_zip $BIDS_dir $subject_ID "/mTI16_800-3800_tgse_pcasl_3.4x3.4x4_14_31_2_24slc/"		ASL_ASL
 
-# SWI-mIP
-# ++++++++++++++++++++++++++++++
-zip_path_kword1="/greME9_p31"
-zip_path_kword2="SWI_mIP_Combined/"
-modality_name=SWI_mIP
-deal_with_3D $1 $2 $3 \
-				$zip_path_kword1 $zip_path_kword2 $modality_name
 
-# SWI-magnitude
-# ++++++++++++++++++++++++++++++
-zip_path_kword1="/greME9_p31"
-zip_path_kword2="_Mag/"
-modality_name=SWI_mag
-deal_with_4D $1 $2 $3 \
-				$zip_path_kword1 $zip_path_kword2 $modality_name
-
-# SWI-phase
-# ++++++++++++++++++++++++++++++
-zip_path_kword1="/greME9_p31"
-zip_path_kword2="_Pha/"
-modality_name=SWI_pha
-deal_with_4D $1 $2 $3 \
-				$zip_path_kword1 $zip_path_kword2 $modality_name
-
-# ASL-relCBF
-# ++++++++++++++++++++++++++++++
-zip_path_kword1="/relCBF/"
-zip_path_kword2="/relCBF/"
-modality_name=ASL_relCBF
-deal_with_3D $1 $2 $3 \
-				$zip_path_kword1 $zip_path_kword2 $modality_name
-
-# ASL-BAT
-# ++++++++++++++++++++++++++++++
-zip_path_kword1="/BAT/"
-zip_path_kword2="/BAT/"
-modality_name=ASL_BAT
-deal_with_3D $1 $2 $3 \
-				$zip_path_kword1 $zip_path_kword2 $modality_name
-
-# ASL-PWI
-# ++++++++++++++++++++++++++++++
-zip_path_kword1="/Perfusion_Weighted/"
-zip_path_kword2="/Perfusion_Weighted/"
-modality_name=ASL_PWI
-deal_with_4D $1 $2 $3 \
-				$zip_path_kword1 $zip_path_kword2 $modality_name
-
-# ASL-ASL
-# ++++++++++++++++++++++++++++++
-zip_path_kword1="/mTI16"
-zip_path_kword2="pcasl"
-modality_name=ASL_ASL
-deal_with_4D $1 $2 $3 \
-				$zip_path_kword1 $zip_path_kword2 $modality_name
-
-				
