1901fc1772cb8b4342d9c51a56bdefb3d0822985
Jiyang Jiang <54608573+JiyangJiang@users.noreply.github.com>
2023-10-27 20:53:10 +1100

    upd

--- BIDS/bmp_BIDS_CHeBA_reorganiseFlywheelDicomZip.sh
+++ BIDS/bmp_BIDS_CHeBA_reorganiseFlywheelDicomZip.sh
@@ -19,16 +19,16 @@ deal_with_3D(){
 
 	echo $DICOM
 
-	# # curr_rand_str=$(LC_CTYPE=C tr -dc 'a-zA-Z0-9' </dev/urandom | head -c 13 ; echo '')
-	# curr_rand_str=$(head -c 5 /dev/random | openssl base64 | sed 's/\///g')
+	# curr_rand_str=$(LC_CTYPE=C tr -dc 'a-zA-Z0-9' </dev/urandom | head -c 13 ; echo '')
+	curr_rand_str=$(head -c 5 /dev/random | openssl base64 | sed 's/\///g')
 
-	# echo "unzip -o -j $DICOM_zip -d $BIDS_dir/sourcedata/$subject_ID/$modality_name $(echo $DICOM | sed 's/ /\\ /g')" > $BMP_TMP_PATH/bmp_${curr_rand_str}.sh
+	echo "unzip -o -j $DICOM_zip -d $BIDS_dir/sourcedata/$subject_ID/$modality_name $(echo $DICOM | sed 's/ /\\ /g')" > $BMP_TMP_PATH/bmp_${curr_rand_str}.sh
 
-	# mkdir -p $BIDS_dir/sourcedata/$subject_ID/$modality_name
+	mkdir -p $BIDS_dir/sourcedata/$subject_ID/$modality_name
 
-	# bash $BMP_TMP_PATH/bmp_${curr_rand_str}.sh && rm -f $BMP_TMP_PATH/bmp_${curr_rand_str}.sh
+	bash $BMP_TMP_PATH/bmp_${curr_rand_str}.sh && rm -f $BMP_TMP_PATH/bmp_${curr_rand_str}.sh
 
-	# mv "$BIDS_dir/sourcedata/$subject_ID/$modality_name/$(echo $DICOM | awk -F/ '{print $NF}')" "$BIDS_dir/sourcedata/$subject_ID/$modality_name/${modality_name}.dcm"
+	mv "$BIDS_dir/sourcedata/$subject_ID/$modality_name/$(echo $DICOM | awk -F/ '{print $NF}')" "$BIDS_dir/sourcedata/$subject_ID/$modality_name/${modality_name}.dcm"
 }
 
 deal_with_4D(){
@@ -43,26 +43,24 @@ deal_with_4D(){
 
 	echo $DICOM
 
-	# # curr_rand_str=$(LC_CTYPE=C tr -dc 'a-zA-Z0-9' </dev/urandom | head -c 13 ; echo '')
-	# curr_rand_str=$(head -c 5 /dev/random | openssl base64 | sed 's/\///g')
+	# curr_rand_str=$(LC_CTYPE=C tr -dc 'a-zA-Z0-9' </dev/urandom | head -c 13 ; echo '')
+	curr_rand_str=$(head -c 5 /dev/random | openssl base64 | sed 's/\///g')
 
-	# echo "unzip -o -j $DICOM_zip -d $BIDS_dir/sourcedata/$subject_ID/$modality_name $(echo $DICOM | sed 's/ /\\ /g')" > $BMP_TMP_PATH/bmp_${curr_rand_str}.sh
+	echo "unzip -o -j $DICOM_zip -d $BIDS_dir/sourcedata/$subject_ID/$modality_name $(echo $DICOM | sed 's/ /\\ /g')" > $BMP_TMP_PATH/bmp_${curr_rand_str}.sh
 
-	# mkdir -p $BIDS_dir/sourcedata/$subject_ID/$modality_name
+	mkdir -p $BIDS_dir/sourcedata/$subject_ID/$modality_name
 
-	# bash $BMP_TMP_PATH/bmp_${curr_rand_str}.sh && rm -f $BMP_TMP_PATH/bmp_${curr_rand_str}.sh
+	bash $BMP_TMP_PATH/bmp_${curr_rand_str}.sh && rm -f $BMP_TMP_PATH/bmp_${curr_rand_str}.sh
 
-	# mv "$BIDS_dir/sourcedata/$subject_ID/$modality_name/$(echo $DICOM | awk -F/ '{print $NF}')" "$BIDS_dir/sourcedata/$subject_ID/$modality_name/${modality_name}.dicom.zip"
+	mv "$BIDS_dir/sourcedata/$subject_ID/$modality_name/$(echo $DICOM | awk -F/ '{print $NF}')" "$BIDS_dir/sourcedata/$subject_ID/$modality_name/${modality_name}.dicom.zip"
 
-	# unzip -o -d $BIDS_dir/sourcedata/$subject_ID/$modality_name "$BIDS_dir/sourcedata/$subject_ID/$modality_name/${modality_name}.dicom.zip" && rm -f "$BIDS_dir/sourcedata/$subject_ID/$modality_name/${modality_name}.dicom.zip"
+	unzip -o -d $BIDS_dir/sourcedata/$subject_ID/$modality_name "$BIDS_dir/sourcedata/$subject_ID/$modality_name/${modality_name}.dicom.zip" && rm -f "$BIDS_dir/sourcedata/$subject_ID/$modality_name/${modality_name}.dicom.zip"
 }
 
 
 DICOM_zip=$1
 BIDS_dir=$2
 subject_ID=$3
-
-
 
 deal_with_3D $DICOM_zip $BIDS_dir $subject_ID "/ABCD_T1w_MPR_vNav_BW740 RMS/"							MEMPRAGE_RMS
 deal_with_4D $DICOM_zip $BIDS_dir $subject_ID "/ABCD_T1w_MPR_vNav_BW740/"								MEMPRAGE_echoes
@@ -72,8 +70,8 @@ deal_with_4D $DICOM_zip $BIDS_dir $subject_ID "/AP_BLOCK_1_DIFFUSION_30DIR/"				
 deal_with_4D $DICOM_zip $BIDS_dir $subject_ID "/AP_BLOCK_2_DIFFUSION_30DIR/"							DWI_AP_2
 deal_with_4D $DICOM_zip $BIDS_dir $subject_ID "/PA_BLOCK_1_DIFFUSION_30DIR/"							DWI_PA_1
 deal_with_4D $DICOM_zip $BIDS_dir $subject_ID "/PA_BLOCK_2_DIFFUSION_30DIR/"							DWI_PA_2
-deal_with_4D $DICOM_zip $BIDS_dir $subject_ID "/AP_FMAP_for DIFFUSION/"									DWI_B0_AP
-deal_with_4D $DICOM_zip $BIDS_dir $subject_ID "/PA_FMAP_for DIFFUSION/"									DWI_B0_PA
+deal_with_4D $DICOM_zip $BIDS_dir $subject_ID "/AP_FMAP_for DIFFUSION/"									DWI_FMAP_AP
+deal_with_4D $DICOM_zip $BIDS_dir $subject_ID "/PA_FMAP_for DIFFUSION/"									DWI_FMAP_PA
 deal_with_3D $DICOM_zip $BIDS_dir $subject_ID "/greME9_p31_256_Iso1mm_Qsm/"								SWI_QSM
 deal_with_3D $DICOM_zip $BIDS_dir $subject_ID "/greME9_p31_256_Iso1mm_SWI_Combined/"					SWI_SWI
 deal_with_3D $DICOM_zip $BIDS_dir $subject_ID "/greME9_p31_256_Iso1mm_SWI_mIP_Combined/"				SWI_mIP
@@ -83,5 +81,15 @@ deal_with_3D $DICOM_zip $BIDS_dir $subject_ID "/relCBF/"												ASL_relCBF
 deal_with_3D $DICOM_zip $BIDS_dir $subject_ID "/BAT/"													ASL_BAT
 deal_with_4D $DICOM_zip $BIDS_dir $subject_ID "/Perfusion_Weighted/"									ASL_PWI
 deal_with_4D $DICOM_zip $BIDS_dir $subject_ID "/mTI16_800-3800_tgse_pcasl_3.4x3.4x4_14_31_2_24slc/"		ASL_ASL
-
+deal_with_4D $DICOM_zip $BIDS_dir $subject_ID "/AP_FMAP pcasl/"											ASL_FMAP_AP
+deal_with_4D $DICOM_zip $BIDS_dir $subject_ID "/PA_FMAP pcasl/"											ASL_FMAP_PA
+deal_with_4D $DICOM_zip $BIDS_dir $subject_ID "/CVR_ep2d_bold 3.8mm TR1500 adaptive/"					CVR
+deal_with_4D $DICOM_zip $BIDS_dir $subject_ID "/AP_FMAP cvr/"											CVR_FMAP_AP
+deal_with_4D $DICOM_zip $BIDS_dir $subject_ID "/PA_FMAP cvr/"											CVR_FMAP_PA
+deal_with_3D $DICOM_zip $BIDS_dir $subject_ID "/t1_mp2rage_sag_0.8x0.8x2_BW240_INV1/"					MP2RAGE_INV1
+deal_with_3D $DICOM_zip $BIDS_dir $subject_ID "/t1_mp2rage_sag_0.8x0.8x2_BW240_INV2/"					MP2RAGE_INV2
+deal_with_3D $DICOM_zip $BIDS_dir $subject_ID "/t1_mp2rage_sag_0.8x0.8x2_BW240_UNI_Images/"				MP2RAGE_UNI
+deal_with_3D $DICOM_zip $BIDS_dir $subject_ID "/t1_mp2rage_sag_0.8x0.8x2_BW240_T1_Images/"				MP2RAGE_T1
+deal_with_3D $DICOM_zip $BIDS_dir $subject_ID "/B1Map_for_T1_mapping/"									B1
+deal_with_4D $DICOM_zip $BIDS_dir $subject_ID "/t1_vibe_sag_DCE_2mm XL FOV 40s temporal res/"			DCE
 
