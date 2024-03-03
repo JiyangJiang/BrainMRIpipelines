1dad0b370b4cb11cb50c22f02cd2e4b3f6326f54
Jiyang Jiang <54608573+JiyangJiang@users.noreply.github.com>
2024-03-03 22:54:13 +1100

    Update preproc_before_BASIL.sh

--- ASL/202403_preparingASLdatasets/preproc_before_BASIL.sh
+++ ASL/202403_preparingASLdatasets/preproc_before_BASIL.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 # Data stored in /data_int/jiyang/tmp/work on GRID
+#
+# NOTES
+#
+# 03/03/2024 - Comparing CSF reference from my own ventricular mask vs.
+#              the one automatically generated from BASIL
 
 work_dir=/data_int/jiyang/tmp/work
 
@@ -16,3 +21,5 @@ for_each -nthreads 40 ${work_dir}/PASL/*-* : gunzip IN/*.gz
 # segment lateral ventricles
 for_each -nthreads 40 ${work_dir}/P*/*-* : mkdir -p IN/ventricle
 for_each -nthreads 40 ${work_dir}/P*/*-* : matlab -nodesktop -nodisplay -r \"addpath\(fullfile\(getenv\(\'BMP_PATH\'\),\'misc\'\)\)\;bmp_misc_getLatVent\(\'IN/m0.nii\',\'IN/t1.nii\',\'IN/ventricle\'\)\;exit\"
+for_each -nthreads 40 ${work_dir}/P*/*-* : cp IN/ventricle/rventricular_mask.nii IN/vent.nii
+for_each -nthreads 40 ${work_dir}/P*/*-* : fslmaths IN/vent -kernel boxv 2 -ero IN/vent_ero
