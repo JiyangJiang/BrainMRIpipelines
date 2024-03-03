9fa1138ba04620965cf0c4024b1230c71a4f277a
Jiyang Jiang <54608573+JiyangJiang@users.noreply.github.com>
2024-03-04 00:45:07 +1100

    Update preproc_before_BASIL.sh

--- ASL/202403_preparingASLdatasets/preproc_before_BASIL.sh
+++ ASL/202403_preparingASLdatasets/preproc_before_BASIL.sh
@@ -23,3 +23,13 @@ for_each -nthreads 40 ${work_dir}/P*/*-* : mkdir -p IN/ventricle
 for_each -nthreads 40 ${work_dir}/P*/*-* : matlab -nodesktop -nodisplay -r \"addpath\(fullfile\(getenv\(\'BMP_PATH\'\),\'misc\'\)\)\;bmp_misc_getLatVent\(\'IN/m0.nii\',\'IN/t1.nii\',\'IN/ventricle\'\)\;exit\"
 for_each -nthreads 40 ${work_dir}/P*/*-* : cp IN/ventricle/rventricular_mask.nii IN/vent.nii
 for_each -nthreads 40 ${work_dir}/P*/*-* : fslmaths IN/vent -kernel boxv 2 -ero IN/vent_ero
+
+# PASL - automatically generated CSF mask
+work_dir=/data_int/jiyang/tmp/work
+for_each -nthreads 40 ${work_dir}/PASL/*-* : mkdir -p IN/basil_autoCSFmask
+for_each -nthreads 40 ${work_dir}/PASL/*-* : oxford_asl -i=IN/asl.nii --iaf=tc --ibf=rpt --bolus=0.7 --rpts=50 --slicedt=0.04667 --tis=1.8 --fslanat=IN/t1.anat -c=IN/m0.nii --cmethod=single --tr=2.5 --cgain=1 --tissref=csf --t1csf=4.3 --t2csf=750 --t2bl=150 --te=11 -o=IN/basil_autoCSFmask --bat=0.7 --t1=1.3 --t1b=1.65 --alpha=0.98 --spatial=1 --mc --pvcorr --artoff
+
+# PASL - my own eroded CSF mask
+work_dir=/data_int/jiyang/tmp/work
+for_each -nthreads 40 ${work_dir}/PASL/*-* : mkdir -p IN/basil_myOwnErodedCSFmask
+oxford_asl -i=IN/asl.nii --iaf=tc --ibf=rpt --bolus=0.7 --rpts=50 --slicedt=0.04667 --tis=1.8 --fslanat=IN/t1.anat -c=IN/m0.nii --cmethod=single --tr=2.5 --cgain=1 --tissref=csf --csf=IN/vent_ero.nii.gz --t1csf=4.3 --t2csf=750 --t2bl=150 --te=11 -o=IN/basil_myOwnErodedCSFmask --bat=0.7 --t1=1.3 --t1b=1.65 --alpha=0.98 --spatial=1 --mc --pvcorr --artoff
