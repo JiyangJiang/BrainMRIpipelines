6f477bd0e710a81711a2cddf5b3af5e475c0ba84
Jiyang Jiang <54608573+JiyangJiang@users.noreply.github.com>
2024-03-13 14:34:54 +1100

    Update bmp_BIDS_CHeBA_chkIntendedFor.m

--- BIDS/bmp_BIDS_CHeBA_chkIntendedFor.m
+++ BIDS/bmp_BIDS_CHeBA_chkIntendedFor.m
@@ -31,11 +31,13 @@ m0scan_json_txt = char(m0scan_json_raw');
 fclose(m0scan_json_fid); 
 m0scan_json_struct = jsondecode (m0scan_json_txt);
 if isempty (m0scan_json_struct.IntendedFor)
+	fprintf ('Reversed PE m0scan json has empty IntendedFor. Correcting ... ');
 	m0scan_json_struct.IntendedFor = string ([""]);
 	m0scan_json_struct.IntendedFor(1,1) = fullfile (['bids::sub-' subject_ID], 'perf', ['sub-' subject_ID '_dir-PA_asl.nii.gz']);
 	m0scan_json_fid = fopen(fullfile(fmap_dir, ['sub-' subject_ID '_dir-AP_m0scan.json']), 'w');
 	fprintf(m0scan_json_fid, '%s', jsonencode(m0scan_json_struct,PrettyPrint=true));
 	fclose(m0scan_json_fid);
+	fprintf ('DONE!\n');
 end
 varargout{1} = m0scan_json_struct;
 fprintf ("DONE!\n");
@@ -52,12 +54,14 @@ if strcmp (study,'VCI')
 		fclose(cvr_ap_json_fid);
 		cvr_ap_json_struct = jsondecode(cvr_ap_json_txt);
 		if isempty (cvr_ap_json_struct.IntendedFor)
+			fprintf ('Json of AP PE fmap for CVR has empty IntendedFor. Correcting ... ');
 			cvr_ap_json_struct.IntendedFor = string (["";""]); % initiate 2*1 string array
 			cvr_ap_json_struct.IntendedFor(1,1) = fullfile (['bids::sub-' subject_ID], 'func', ['sub-' subject_ID '_task-rest_dir-PA_bold.nii.gz']);
 			cvr_ap_json_struct.IntendedFor(2,1) = fullfile (['bids::sub-' subject_ID], 'func', ['sub-' subject_ID '_task-co2_dir-PA_bold.nii.gz']);
 			cvr_ap_json_fid = fopen(fullfile(fmap_dir, ['sub-' subject_ID '_acq-APforCVR_dir-AP_epi.json']), 'w');
 			fprintf(cvr_ap_json_fid, '%s', jsonencode(cvr_ap_json_struct, PrettyPrint=true));
 			fclose(cvr_ap_json_fid);
+			fprintf ('DONE!\n');
 		end
 		varargout{end+1} = cvr_ap_json_struct;
 		fprintf ("DONE!\n");
@@ -75,12 +79,14 @@ if strcmp (study,'VCI')
 		fclose(cvr_pa_json_fid);
 		cvr_pa_json_struct = jsondecode(cvr_pa_json_txt);
 		if isempty (cvr_pa_json_struct.IntendedFor)
+			fprintf ('Json of PA PE fmap for CVR has empty IntendedFor. Correcting ... ');
 			cvr_pa_json_struct.IntendedFor = string (["";""]); % initiate 2*1 string array
 			cvr_pa_json_struct.IntendedFor(1,1) = fullfile (['bids::sub-' subject_ID], 'func', ['sub-' subject_ID '_task-rest_dir-PA_bold.nii.gz']);
 			cvr_pa_json_struct.IntendedFor(2,1) = fullfile (['bids::sub-' subject_ID], 'func', ['sub-' subject_ID '_task-co2_dir-PA_bold.nii.gz']);
 			cvr_pa_json_fid = fopen(fullfile(fmap_dir, ['sub-' subject_ID '_acq-PAforCVR_dir-PA_epi.json']), 'w');
 			fprintf(cvr_pa_json_fid, '%s', jsonencode(cvr_pa_json_struct, PrettyPrint=true));
 			fclose(cvr_pa_json_fid);
+			fprintf ('DONE!\n');
 		end
 		varargout{end+1} = cvr_pa_json_struct;
 		fprintf("DONE!\n");
@@ -100,11 +106,13 @@ elseif strcmp (study,'CADSYD')
 		fclose(rsfmri_ap_json_fid);
 		rsfmri_ap_json_struct = jsondecode(rsfmri_ap_json_txt);
 		if isempty (rsfmri_ap_json_struct.IntendedFor)
+			fprintf ('Json of AP PE fmap for rsfMRI has empty IntendedFor. Correcting ... ');
 			rsfmri_ap_json_struct.IntendedFor = string ([""]);
 			rsfmri_ap_json_struct.IntendedFor(1,1) = fullfile (['bids::sub-' subject_ID], 'func', ['sub-' subject_ID '_task-rest_dir-PA_bold.nii.gz']);
 			rsfmri_ap_json_fid = fopen(fullfile(fmap_dir, ['sub-' subject_ID '_acq-pepolarForRsfmri_dir-AP_epi.json']), 'w');
 			fprintf(rsfmri_ap_json_fid, '%s', jsonencode(rsfmri_ap_json_struct, PrettyPrint=true));
 			fclose(rsfmri_ap_json_fid);
+			fprintf ('DONE!\n');
 		end
 		varargout{end+1} = rsfmri_ap_json_struct;
 		fprintf("DONE!\n");
@@ -122,11 +130,13 @@ elseif strcmp (study,'CADSYD')
 		fclose(rsfmri_pa_json_fid);
 		rsfmri_pa_json_struct = jsondecode(rsfmri_pa_json_txt);
 		if isempty (rsfmri_pa_json_struct.IntendedFor)
+			fprintf ('Json of PA PE fmap for rsfMRI has empty IntendedFor. Correcting ... ');
 			rsfmri_pa_json_struct.IntendedFor = string ([""]);
 			rsfmri_pa_json_struct.IntendedFor(1,1) = fullfile (['bids::sub-' subject_ID], 'func', ['sub-' subject_ID '_task-rest_dir-PA_bold.nii.gz']);
 			rsfmri_pa_json_fid = fopen(fullfile(fmap_dir, ['sub-' subject_ID '_acq-pepolarForRsfmri_dir-PA_epi.json']), 'w');
 			fprintf(rsfmri_pa_json_fid, '%s', jsonencode(rsfmri_pa_json_struct, PrettyPrint=true));
 			fclose(rsfmri_pa_json_fid);
+			fprintf ('DONE!\n');
 		end
 		varargout{end+1} = rsfmri_pa_json_struct;
 		fprintf("DONE!\n");
