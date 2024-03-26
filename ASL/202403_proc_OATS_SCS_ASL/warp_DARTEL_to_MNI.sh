0c1e98b64afef57280ccbf61f423b8f89cba6262
Jiyang Jiang <54608573+JiyangJiang@users.noreply.github.com>
2024-03-27 03:28:04 +1100

    Update warp_DARTEL_to_MNI.sh

--- ASL/202403_proc_OATS_SCS_ASL/warp_DARTEL_to_MNI.sh
+++ ASL/202403_proc_OATS_SCS_ASL/warp_DARTEL_to_MNI.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 
+invwarp --ref=${FSLDIR}/data/standard/MNI152_T1_2mm --warp=${BMP_PATH}/ASL/202403_proc_OATS_SCS_ASL/MNI2DARTEL_flowMap_65to75 --out=${BMP_PATH}/ASL/202403_proc_OATS_SCS_ASL/DARTEL2MNI_flowMap_65to75
+
+
+
+applywarp --ref=${FSLDIR}/data/standard/MNI152_T1_2mm --in=my_structural --warp=my_nonlinear_transf --out=my_warped_structural
